7793fb188a89095927bd3930468d7ea4b8006946
Chris V. <vochris81@gmail.com>
2025-10-02 09:20:20 -0400

    Fix input prompt and update output messages

--- talha-initial-quiz/initial-quiz-question-2.cpp
+++ talha-initial-quiz/initial-quiz-question-2.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
+#include <limits>
 using namespace std;
 
 int main() {
     char user_input;
 
-    cout << "Enter 1 a character, anything form number to letter (upper/lower case) :" << endl;
+    cout << "Enter a character, anything from number to letter (upper/lower case) :" << endl;
     cin >> user_input;
     while (cin.fail()) {
         cin.clear();
@@ -18,17 +19,17 @@ int main() {
     cout << "You entered : " << user_input << endl;
 
     if (user_input >= '0' && user_input <= '9') {
-        if (user_input % 2 == 0) {
-            cout << "This is an EVEN NUMBER" << endl;
+        if ((user_input - '0') % 2 == 0) {
+            cout << "This is a EVEN NUMBER" << endl;
         } else {
-            cout << "This is an ODD NUMBER" << endl;
+            cout << "This is a ODD NUMBER" << endl;
         }
     } else if (user_input >= 'a' && user_input <= 'z') {
-        cout << "This is an LOWER-CASE LETTER" << endl;
+        cout << "This is a LOWER-CASE LETTER" << endl;
     } else if (user_input >= 'A' && user_input <= 'Z') {
-        cout << "This is an UPPER-CASE LETTER" << endl;
+        cout << "This is a UPPER-CASE LETTER" << endl;
     } else {
-        cout << "This is an SPECIAL CHARACTER" << endl;
+        cout << "This is a SPECIAL CHARACTER" << endl;
     }
 
     return 0;
