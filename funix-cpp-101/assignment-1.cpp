92bd59ce603e9e92d8e3700b12501afc6bf74bbb
Chris V. <vochris81@gmail.com>
2025-12-16 15:38:54 +0000

    move files and init assignment 1

--- funix-cpp-101/assignment-1.cpp
+++ funix-cpp-101/assignment-1.cpp
@@ -3,5 +3,17 @@ using namespace std;
 
 int main()
 {
-  return 0
-}
+  int day, month, year, hour, minute;
+  cout << "Nhap ngay: ";
+  cin >> day;
+  cout << "Nhap thang: ";
+  cin >> month;
+  cout << "Nhap nam: ";
+  cin >> year;
+  cout << "Nhap gio: ";
+  cin >> hour;
+  cout << "Nhap phut: ";
+  cin >> minute;
+
+  return 0;
+};
