af53ee4d0ddd32a055dfb6a21a0e65e4774e8e5d
Chris V. <vochris81@gmail.com>
2025-12-16 21:46:39 +0000

    Refactor time setting program to improve input prompts and validation functions for year, month, day, hour, and minute

--- funix-cpp-101/asn-1_setting-time-for-car/assignment-1.cpp
+++ funix-cpp-101/asn-1_setting-time-for-car/assignment-1.cpp
@@ -1,19 +1,81 @@
 #include <iostream>
 using namespace std;
 
+int validate_year(int year)
+{
+  while (year <= 0 || year > 2025)
+  {
+    cout << "Invalid year. Please enter again: ";
+    cin >> year;
+  }
+  return year;
+}
+
+int validate_month(int month)
+{
+  while (month < 1 || month > 12)
+  {
+    cout << "Invalid month. Please enter again: ";
+    cin >> month;
+  }
+  return month;
+}
+
+int validate_day(int day)
+{
+  while (day < 1 || day > 31)
+  {
+    cout << "Invalid day. Please enter again: ";
+    cin >> day;
+  }
+  return day;
+}
+
+int validate_hour(int hour)
+{
+  while (hour < 0 || hour > 23)
+  {
+    cout << "Invalid hour. Please enter again: ";
+    cin >> hour;
+  }
+  return hour;
+}
+
+int validate_minute(int minute)
+{
+  while (minute < 0 || minute > 59)
+  {
+    cout << "Invalid minute. Please enter again: ";
+    cin >> minute;
+  }
+  return minute;
+}
+
 int main()
 {
+  cout << "Program to set time for car" << endl;
+  cout << "Enter following information" << endl;
+
   int day, month, year, hour, minute;
-  cout << "Nhap ngay: ";
+  cout << "   Day: ";
   cin >> day;
-  cout << "Nhap thang: ";
+  day = validate_day(day);
+
+  cout << "   Month: ";
   cin >> month;
-  cout << "Nhap nam: ";
+  month = validate_month(month);
+
+  cout << "   Year: ";
   cin >> year;
-  cout << "Nhap gio: ";
+  year = validate_year(year);
+  
+  cout << "   Hour: ";
   cin >> hour;
-  cout << "Nhap phut: ";
+  hour = validate_hour(hour);
+
+  cout << "   Minute: ";
   cin >> minute;
+  minute = validate_minute(minute);
 
   return 0;
 };
