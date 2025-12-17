a24c2dbd19e3afee61e7c6a6e979eed6314b13cd
chrv81 <vochris81@gmail.com>
2025-12-17 00:56:29 -0500

    add display date

--- funix-cpp-101/asn-1_setting-time-for-car/assignment-1.cpp
+++ funix-cpp-101/asn-1_setting-time-for-car/assignment-1.cpp
@@ -14,24 +14,37 @@ string get_month_name(int month)
 {
   switch (month)
   {
-    case 1: return "Jan";
-    case 2: return "Feb";
-    case 3: return "Mar";
-    case 4: return "Apr";
-    case 5: return "May";
-    case 6: return "Jun";
-    case 7: return "Jul";
-    case 8: return "Aug";
-    case 9: return "Sep";
-    case 10: return "Oct";
-    case 11: return "Nov";
-    case 12: return "Dec";
-    default: return "";
+  case 1: return "Jan";
+  case 2: return "Feb";
+  case 3: return "Mar";
+  case 4: return "Apr";
+  case 5: return "May";
+  case 6: return "Jun";
+  case 7: return "Jul";
+  case 8: return "Aug";
+  case 9: return "Sep";
+  case 10: return "Oct";
+  case 11: return "Nov";
+  case 12: return "Dec";
+  default: return "";
   }
 }
 
-// 
-
+// Get # of days in a month
+int get_days_in_mont(int month, int year)
+{
+  if (month == 2)
+  {
+    // Feb has 28 days (if it's a leap year, it has 29 days)
+    return is_leap_year(year) ? 29 : 28;
+  }
+  // Months with 31 days are 1, 3, 5, 7, 8, 10, 12
+  if ((month <= 7 && month % 2 != 0) || (month >= 8 && month % 2 == 0))
+  {
+    return 31;
+  }
+  return 30;
+}
 
 // Validate functions for user inputs
 int validate_year(int year)
@@ -54,9 +67,10 @@ int validate_month(int month)
   return month;
 }
 
-int validate_day(int day)
+int validate_day(int day, int month, int year)
 {
-  while (day < 1 || day > 31)
+  int max_days = get_days_in_mont(month, year);
+  while (day < 1 || day > max_days)
   {
     cout << "Invalid day. Please enter again: ";
     cin >> day;
@@ -84,24 +98,34 @@ int validate_minute(int minute)
   return minute;
 }
 
+void display_date_format(int hr, int min, int dd, int mm, int yy)
+{
+  string month = get_month_name(mm);
+  cout << hr << "h:" << min << "m, " << dd << ", " << month << ", " << yy << endl;
+}
+
 int main()
 {
+  cout << "========================================" << endl;
   cout << "Program to set time for car" << endl;
+  cout << "========================================" << endl;
   cout << "Enter following information" << endl;
 
+  // init data
   int day, month, year, hour, minute;
-  cout << "   Day: ";
-  cin >> day;
-  day = validate_day(day);
+
+  cout << "   Year: ";
+  cin >> year;
+  year = validate_year(year);
 
   cout << "   Month: ";
   cin >> month;
   month = validate_month(month);
 
-  cout << "   Year: ";
-  cin >> year;
-  year = validate_year(year);
-  
+  cout << "   Day: ";
+  cin >> day;
+  day = validate_day(month, day, year);
+
   cout << "   Hour: ";
   cin >> hour;
   hour = validate_hour(hour);
@@ -109,6 +133,8 @@ int main()
   cout << "   Minute: ";
   cin >> minute;
   minute = validate_minute(minute);
+
+  display_date_format(hour, minute, day, month, year);
 
   return 0;
 };
