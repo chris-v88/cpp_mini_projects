aa8c361a6cfdb2a56cdbf6c6a2f24cc878fe75e7
chrv81 <vochris81@gmail.com>
2025-12-20 21:10:06 -0500

    update discount

--- funix-cpp-101/lab-6/discount-receipt.cpp
+++ funix-cpp-101/lab-6/discount-receipt.cpp
@@ -5,26 +5,29 @@ using namespace std;
 Lab 6.5: Tính số tiền chiết khấu hóa đơn dựa trên hàm có sẵn:
     Đầu vào input: số tiền
     Đầu ra output: In ra số tiền phải trả sau khi được chiết khấu
+
 Biết:
     Nếu số tiền >5000 thì chiết khấu là 20%
     Nếu 2000 <= số tiền <= 5000 thì chiết khấu là 10%
     Nếu số tiền < 2000 thì chiết khấu là 5%
 */
 
-void discount(int amount)
+float discount(int amount)
 {
     float disAmount;
+    float total = amount;
     // write nested conditions to print discounted amount
     if (amount > 5000)
     {
-        cout << "%20" << endl;
-    } else if (amount >= 2000 || amount <= 5000)
+        disAmount = 0.20;
+    } else if (amount >= 2000 && amount <= 5000)
     {
-        cout << "50%" << endl;
+        disAmount = 0.10;
     } else
     {
-        cout << "5%" << endl;
+        disAmount = 0.05;
     }
+    return total * (1 - disAmount);
 }
 
 int main()
@@ -33,5 +36,6 @@ int main()
     cout << "Enter the amount: ";
     int amount;
     cin >> amount;
-    discount(amount);
+    float total = discount(amount);
+    cout << "Amount to pay after discount: " << total;
 }
