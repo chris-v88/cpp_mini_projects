f245b9b04124200dd3cb892c141e50f6e21a54c3
chrv81 <vochris81@gmail.com>
2025-12-20 19:25:29 -0500

    exercise 1 - integer sign

---
+++ funix-cpp-101/lab-6/interger-sign.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void sign(int n)
+{
+    //write conditional statement here to print "positive" or "negative"
+    if (n < 0) {
+        cout << "   --> This is a NEGATIVE number" << endl;;
+    }
+    else if (n > 0) {
+        cout << "   --> This is a POSITIVE number" << endl;;
+    }
+    else {
+        cout << "   --> This is ZERO" << endl;
+    }
+}
+
+int main()
+{
+    cout << "------- INTERGER SIGN CHECK -------" << endl;
+    cout << "Enter a number, the program will determine if it is positive or negative: " << endl;
+    int num;
+    cin >> num;
+    sign(num);
+    return 0;
+}
